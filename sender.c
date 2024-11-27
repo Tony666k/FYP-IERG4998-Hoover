1fb905b65f1bd8bf2af8e85d3b6e7312526b3bb1
SummerHEHEHE <150149707+SummerHEHEHE@users.noreply.github.com>
2024-11-27 22:46:48 +0800

    Update sender.c

--- sender.c
+++ sender.c
@@ -11,12 +11,13 @@
 void send_udp_message(const char *source_ip,int source_port,const char *target_ip,int target_port){
 int sockfd;
 struct sockaddr_in server_addr,target_addr;
-char message[]="MPUDP Test Message";
+char message[] = rand() % 100 + 1;
+
 
 //create socket
 if((sockfd=socket(AF_INET,SOCK_DGRAM,0))<0){
 perror("Socket creation failed");
-exit(EXIT_FAILURE);
+exit(EXIT_FAILURE);//exit(1);
 }
 
 //set source addr
@@ -44,18 +45,23 @@ close(sockfd);
 }
 
 int main(){
- const char *target_ip="192.168.84.129";
- const char *source_ip="192.168.84.130";
- int source_ports[]={10000,10001};
+ const char *target_ip="192.168.229.134";
+ const char *source_ip="192.168.84.135";
+ int source_ports[]={10000,10001,10002};
+ //10000 for 1,3,5,7,9;
+ //10001 for 2,4,6,8,10;
+ //10002 for 1 XOR 2......9 XOR 10;
  int target_port=PORT;
  int num_ports=sizeof(source_ports)/sizeof(source_ports[0]);
  srand(time(0));//set random seed
  
 //loop for send data
-while(1){
-int source_port =source_ports[rand()%num_ports];
-send_udp_message(source_ip,source_port,target_ip,target_port);
-sleep(1);//send 1 time per second
+for (int i=1; i<=10; i++){
+if( i % 2 != 0)
+send_udp_message(source_ip,10000,target_ip,target_port);
+if( i % 2 == 0)
+send_udp_message(source_ip,10001,target_ip,target_port);
+//sleep(1);//send 1 time per second
 }
 
 return 0;}
