af18847910969e1d135041acdd21f46f654174f2
Tony666k <x1579840498@gmail.com>
2024-11-28 21:07:42 +0800

    Update testing.c

--- testing.c
+++ testing.c
@@ -5,29 +5,14 @@
 #include <arpa/inet.h>
 #include <pthread.h>
 
-#define PORT 5001 //set the udp port
-#define BUF_SIZE 1024 //set the buffer size 
-#define MESSAGE_LENGTH 16 //define the total msg length (16 bits)
+#define PORT 5001 // 设置UDP端口
+#define BUF_SIZE 1024 // 设置缓冲区大小
+#define MESSAGE_LENGTH 4 // 每个数据包的长度 (4个'1')
 
-// 固定的二进制消息
-#define FIXED_MSG "1111111111111111" // 固定的16个1
+// 固定消息
+#define FIXED_MSG "1111" // 固定的4个'1'
 
-// 将固定二进制消息分割成每4个1为一个小数据包
-void split_message(const char *msg, char *part1, char *part2, char *part3, char *part4) {
-    strncpy(part1, msg, 4);
-    part1[4] = '\0';
-    
-    strncpy(part2, msg + 4, 4);
-    part2[4] = '\0';
-    
-    strncpy(part3, msg + 8, 4);
-    part3[4] = '\0';
-    
-    strncpy(part4, msg + 12, 4);
-    part4[4] = '\0';
-}
-
-// 发送UDP消息
+// 发送UDP消息的函数
 int send_udpmsg(const char *server_ip, int server_port, const char *client_ip, int client_port, const char *message) {
     int sock_descriptor;
     struct sockaddr_in server_addr, client_addr;
@@ -77,36 +62,28 @@ int send_udpmsg(const char *server_ip, int server_port, const char *client_ip, i
     return 0;
 }
 
-// 线程函数：向10000端口发送2个4个1的数据包
+// 线程函数：向10000端口发送2个固定数据包 "1111"
 void* send_to_10000(void *arg) {
     const char *server_ip = "192.168.229.135"; // 服务器IP地址
     const char *client_ip = "192.168.229.134"; // 客户端IP地址
     int client_port = PORT;
 
-    // 固定二进制消息分割
-    char part1[5], part2[5], part3[5], part4[5];
-    split_message(FIXED_MSG, part1, part2, part3, part4);
-
-    // 发送每个分包到10000端口
-    send_udpmsg(server_ip, 10000, client_ip, client_port, part1);
-    send_udpmsg(server_ip, 10000, client_ip, client_port, part2);
+    // 发送两个"1111"的数据包到10000端口
+    send_udpmsg(server_ip, 10000, client_ip, client_port, FIXED_MSG);
+    send_udpmsg(server_ip, 10000, client_ip, client_port, FIXED_MSG);
 
     return NULL;
 }
 
-// 线程函数：向10001端口发送2个4个1的数据包
+// 线程函数：向10001端口发送2个固定数据包 "1111"
 void* send_to_10001(void *arg) {
     const char *server_ip = "192.168.229.135"; // 服务器IP地址
     const char *client_ip = "192.168.229.134"; // 客户端IP地址
     int client_port = PORT;
 
-    // 固定二进制消息分割
-    char part3[5], part4[5];
-    split_message(FIXED_MSG, NULL, NULL, part3, part4);
-
-    // 发送每个分包到10001端口
-    send_udpmsg(server_ip, 10001, client_ip, client_port, part3);
-    send_udpmsg(server_ip, 10001, client_ip, client_port, part4);
+    // 发送两个"1111"的数据包到10001端口
+    send_udpmsg(server_ip, 10001, client_ip, client_port, FIXED_MSG);
+    send_udpmsg(server_ip, 10001, client_ip, client_port, FIXED_MSG);
 
     return NULL;
 }
@@ -117,43 +94,16 @@ void* send_to_10002(void *arg) {
     const char *client_ip = "192.168.229.134"; // 客户端IP地址
     int client_port = PORT;
 
-    // 固定二进制消息分割
-    char part1[5], part2[5], part3[5], part4[5];
-    split_message(FIXED_MSG, part1, part2, part3, part4);
+    // 固定消息 "1111" XOR 运算：对两个数据包进行 XOR
+    char XOR_result[MESSAGE_LENGTH + 1]; // 存储XOR结果
 
-    // XOR运算：对两个数据包进行XOR
-    char XOR_result[5];
-    for (int i = 0; i < 4; i++) {
-        // XOR 第一个和第二个部分
-        XOR_result[i] = (part1[i] == part2[i]) ? '0' : '1';
+    // XOR: "1111" XOR "1111" = "0000"
+    for (int i = 0; i < MESSAGE_LENGTH; i++) {
+        XOR_result[i] = (FIXED_MSG[i] == FIXED_MSG[i]) ? '0' : '1'; // 因为是相同的"1111"，结果是"0000"
     }
-    XOR_result[4] = '\0'; // 添加结束符
+    XOR_result[MESSAGE_LENGTH] = '\0'; // 添加结束符
 
     // 发送XOR结果到10002端口
-    send_udpmsg(server_ip, 10002, client_ip, client_port, XOR_result);
-
-    return NULL;
-}
-
-// 线程函数：计算第二次XOR并发送到10002端口
-void* send_to_10003(void *arg) {
-    const char *server_ip = "192.168.229.135"; // 服务器IP地址
-    const char *client_ip = "192.168.229.134"; // 客户端IP地址
-    int client_port = PORT;
-
-    // 固定二进制消息分割
-    char part3[5], part4[5];
-    split_message(FIXED_MSG, NULL, NULL, part3, part4);
-
-    // XOR运算：对两个数据包进行XOR
-    char XOR_result[5];
-    for (int i = 0; i < 4; i++) {
-        // XOR 第三个和第四个部分
-        XOR_result[i] = (part3[i] == part4[i]) ? '0' : '1';
-    }
-    XOR_result[4] = '\0'; // 添加结束符
-
-    // 发送XOR结果到10003端口
     send_udpmsg(server_ip, 10002, client_ip, client_port, XOR_result);
 
     return NULL;
@@ -164,24 +114,21 @@ int main() {
     const char *client_ip = "192.168.229.134"; // 客户端IP地址
     int client_port = PORT;
 
-    // 创建共享数据结构保存消息（此处不用再使用原结构）
-    
     // 发送4次消息
     for (int i = 0; i < 4; i++) {
-        pthread_t thread_10000, thread_10001, thread_10002, thread_10003;
+        pthread_t thread_10000, thread_10001, thread_10002;
 
         // 创建线程分别发送数据到10000, 10001
         pthread_create(&thread_10000, NULL, send_to_10000, NULL);
         pthread_create(&thread_10001, NULL, send_to_10001, NULL);
 
-        // 线程发送并计算XOR
+        // 计算XOR并发送到10002
         pthread_create(&thread_10002, NULL, send_to_10002, NULL);
-        pthread_create(&thread_10003, NULL, send_to_10003, NULL);
 
+        // 等待线程完成
         pthread_join(thread_10000, NULL);  // 等待thread_10000完成
         pthread_join(thread_10001, NULL);  // 等待thread_10001完成
         pthread_join(thread_10002, NULL);  // 等待thread_10002完成
-        pthread_join(thread_10003, NULL);  // 等待thread_10003完成
 
         // 可选：添加小延时
         // sleep(1);
