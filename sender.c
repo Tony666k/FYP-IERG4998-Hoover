935211d3b1c0178f2b2a0ba3b2daadffab425811
SummerHEHEHE <150149707+SummerHEHEHE@users.noreply.github.com>
2024-11-28 11:04:58 +0800

    Update sender.c

--- sender.c
+++ sender.c
@@ -8,32 +8,51 @@
 
 #define PORT 5001
 #define BUF_SIZE 1024
-#define MESSAGE_LENGTH 88
+#define MESSAGE_LENGTH 8
 
+/*
 unsigned int get_microsecond_seed() {
     struct timeval tv;
     gettimeofday(&tv, NULL);
-    return tv.tv_sec * 1000000 + tv.tv_usec;  // 返回微秒级时间戳
+    return tv.tv_sec * 1000000 + tv.tv_usec;  // return microsecond seed
 }
 
-void generate_random_binary_message(char *bmessage, int length) {
+/*
+void generate_message(char *message, int length) {
     for (int i = 0; i < length; i++) {
-        bmessage[i] = rand() % 2 ? '1' : '0';
+        message[i] = rand() % 2 ? '1' : '0'; // binary message 
     }
-    bmessage[length] = '\0';
+    message[length] = '\0';
+}
+*/
+
+void Binary_choose(char *message, int length){
+    for(int i = 0; i < length; i++)
+        message[i] = rand() % 2 ? '1' : '0'; // bit by bit randomly choose 1 or 0
+    message[length] = '\0';// put in \0 for end of recognition
 }
 
-void send_udp_message(const char *source_ip,int source_port,const char *target_ip,int target_port){
-int sockfd;
-srand(get_microsecond_seed());
-struct sockaddr_in server_addr,target_addr;
-char message[MESSAGE_LENGTH + 1] ;
-generate_random_binary_message(message, MESSAGE_LENGTH);
-char message_with_uid[MESSAGE_LENGTH + 30];
-int uid = rand();
-snprintf(message_with_uid, sizeof(message_with_uid), "UID%d:%s", uid, message);
+int UID(){
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    unsigned int seed = srand(tv.tv_sec * 1000000 + tv.tv_usec)
+    int uid = rand();
+    return uid;
+}
 
+char message_combine(char message*){
+    char binary_message[MESSAGE_LENGTH + 1];
+    Binary_choose(binary_message, MESSAGE_LENGTH);
+    int uid = UID();
+    message[] = snprintf(message_with_uid, sizeof(message_with_uid), "UID(%d):%s", uid, binary_message);
+    return message[];
+    }
+
+void Send_message(const char *source_ip, int source_port, const char *target_ip, int target_port, char message*){
     
+int sockfd;
+struct sockaddr_in server_addr,target_addr;
+
 //create socket
 if((sockfd=socket(AF_INET,SOCK_DGRAM,0))<0){
 perror("Socket creation failed");
@@ -59,7 +78,7 @@ target_addr.sin_addr.s_addr=inet_addr(target_ip);
 target_addr.sin_port=htons(target_port);
 
 //send the data packet
-sendto(sockfd,(const char *)message_with_uid,strlen(message_with_uid),0,(const struct sockaddr *)&target_addr,sizeof(target_addr));
+sendto(sockfd,(const char *)message, strlen(message), 0, (const struct sockaddr *)&target_addr, sizeof(target_addr));
 printf("Message sent from %s:%d to %s:%d UID:%d\n",source_ip,source_port,target_ip,target_port,uid);
 
 close(sockfd);
@@ -69,6 +88,11 @@ int main(){
  const char *target_ip="192.168.229.134";
  const char *source_ip="192.168.229.135";
  int source_ports[]={10000,10001,10002};
+ char message_send_10000[];
+ char message_send_10001[];
+ char message_send_10002[];
+ char message_record_10001[];
+ char message_record_10002[];
  //10000 for 1,3,5,7,9;
  //10001 for 2,4,6,8,10;
  //10002 for 1 XOR 2......9 XOR 10;
@@ -77,13 +101,23 @@ int main(){
  srand(time(0));//set random seed
  
 //loop for send data
-for (int i=1; i<=MESSAGE_LENGTH; i++){
-if( i % 2 != 0)
-send_udp_message(source_ip,10000,target_ip,target_port);
-if( i % 2 == 0)
-send_udp_message(source_ip,10001,target_ip,target_port);
+    for (int i=1; i<=MESSAGE_LENGTH; i++){
+        if( i % 2 != 0){
+            message_send_10000[] = message_combine(message_send_10000[]);
+            message_record_10000[] = message_send_10000[];
+            send_udp_message(source_ip,10000,target_ip,target_port,message_send_10000);
+            }
+        if( i % 2 == 0){
+             message_send_10001[] = message_combine(message_send_10001[]);
+             message_record_10001[] = message_send_10001[];
+             send_udp_message(source_ip,10001,target_ip,target_port,message_send_10001);
+        }
 //sleep(1);//send 1 time per second
+     }
 }
 
-return 0;}
+}
+return 0;
+        
+}
 
